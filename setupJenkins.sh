142c591f5c36017d22261d334e4bf550393b9585
Ekansh Verma <47328709+ekanshverma@users.noreply.github.com>
2019-10-16 11:52:57 +0530

    Update setupJenkins.sh

--- setupJenkins.sh
+++ setupJenkins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt update
-sudo apt install software-properties-common apt-transport-https -y
-sudo add-apt-repository ppa:openjdk-r/ppa -y
+apt install software-properties-common apt-transport-https -y
+add-apt-repository ppa:openjdk-r/ppa -y
 apt install openjdk-8-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-echo 'deb https://pkg.jenkins.io/debian-stable binary/' | tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt install jenkins -y
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt update
+apt install jenkins -y
 systemctl start jenkins
 systemctl enable jenkins
 
